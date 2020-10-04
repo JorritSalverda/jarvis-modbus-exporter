d906627b2e05160c2846b2ebe5f07366fe5d40cc
Jorrit Salverda <jsalverda@travix.com>
2020-10-04 17:10:38 +0200

    use contracts stubs generated with newer protoc-gen-go

--- client/config/client_test.go
+++ client/config/client_test.go
@@ -24,6 +24,6 @@ func TestReadConfigFromFile(t *testing.T) {
 		assert.Equal(t, contractsv1.AggregationLevel_AGGREGATION_LEVEL_DEVICE, config.SampleConfigs[0].AggregationLevel)
 		assert.Equal(t, contractsv1.MetricType_METRIC_TYPE_COUNTER, config.SampleConfigs[0].MetricType)
 		assert.Equal(t, contractsv1.SampleType_SAMPLE_TYPE_ELECTRICITY, config.SampleConfigs[0].SampleType)
-		assert.Equal(t, contractsv1.SampleUnit_SAMPLE_UNIT_WATT_SECOND, config.SampleConfigs[0].SampleUnit)
+		assert.Equal(t, contractsv1.SampleUnit_SAMPLE_UNIT_JOULE, config.SampleConfigs[0].SampleUnit)
 	})
 }
