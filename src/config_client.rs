087d8a823d2c0c1dfd1b6e79b361bf430d79d462
Jorrit Salverda <jorrit.salverda@gmail.com>
2021-04-22 16:42:22 +0200

    test config_client to unmarshal test config yaml file

--- src/config_client.rs
+++ src/config_client.rs
@@ -1,5 +1,8 @@
 use std::env;
 use std::error::Error;
+use std::fs;
+use serde_yaml;
+use crate::model::{Config,EntityType,SampleType,MetricType,RegisterType};
 
 pub struct ConfigClientConfig {
   config_path: String,
@@ -24,5 +27,37 @@ pub struct ConfigClient {
 impl ConfigClient {
   pub fn new(config: ConfigClientConfig) -> Self {
     Self{ config }
+  }
+
+  pub fn read_config_from_file(&self) -> Result<Config, Box<dyn Error>> {
+    let config_file_contents = fs::read_to_string(&self.config.config_path)?;
+    let config: Config = serde_yaml::from_str(&config_file_contents)?;
+
+    Ok(config)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn it_works() {
+
+    let config_client = ConfigClient::new(ConfigClientConfig::new("test-config.yaml".to_string()).unwrap());
+
+    let config = config_client.read_config_from_file().unwrap();
+
+    assert_eq!(config.location, "My Home".to_string());
+    assert_eq!(config.sample_configs.len(), 1);
+    assert_eq!(config.sample_configs[0].entity_type, EntityType::Device);
+    assert_eq!(config.sample_configs[0].entity_name, "Sunny TriPower 8.0".to_string());
+    assert_eq!(config.sample_configs[0].sample_type, SampleType::ElectricityProduction);
+    assert_eq!(config.sample_configs[0].sample_name, "Totaal opgewekt".to_string());
+    assert_eq!(config.sample_configs[0].metric_type, MetricType::Counter);
+    assert_eq!(config.sample_configs[0].value_multiplier, 3600f64);
+    assert_eq!(config.sample_configs[0].register_type, RegisterType::Input);
+    assert_eq!(config.sample_configs[0].register_address, 30513u16);
+    assert_eq!(config.sample_configs[0].register_quantity, 4u16);
   }
 }
