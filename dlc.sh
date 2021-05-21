3281f476f0d6371a79074736c904c7beb186bfd0
jorrit.salverda@gmail.com <jsalverda@travix.com>
2021-05-21 10:53:38 +0200

    add script to build docker caching layers

--- dlc.sh
+++ dlc.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 set -e
 
-docker build \
+DOCKER_BUILDKIT=1 docker build \
 	--target planner \
 	--tag jsalverda/jarvis-modbus-exporter:dlc-main-planner \
 	--cache-from jsalverda/jarvis-modbus-exporter:dlc-main-planner \
 	--build-arg BUILDKIT_INLINE_CACHE=1 .
-docker push jsalverda/jarvis-modbus-exporter:dlc-main-planner
+DOCKER_BUILDKIT=1 docker push jsalverda/jarvis-modbus-exporter:dlc-main-planner
 
-docker build \
+DOCKER_BUILDKIT=1 docker build \
 	--target cacher \
 	--tag jsalverda/jarvis-modbus-exporter:dlc-main-cacher \
 	--cache-from jsalverda/jarvis-modbus-exporter:dlc-main-planner \
 	--cache-from jsalverda/jarvis-modbus-exporter:dlc-main-cacher \
 	--build-arg BUILDKIT_INLINE_CACHE=1 .
-docker push jsalverda/jarvis-modbus-exporter:dlc-main-cacher
+DOCKER_BUILDKIT=1 docker push jsalverda/jarvis-modbus-exporter:dlc-main-cacher
 
-docker build \
+DOCKER_BUILDKIT=1 docker build \
 	--target builder \
 	--tag jsalverda/jarvis-modbus-exporter:dlc-main-builder \
 	--cache-from jsalverda/jarvis-modbus-exporter:dlc-main-planner \
 	--cache-from jsalverda/jarvis-modbus-exporter:dlc-main-cacher \
 	--cache-from jsalverda/jarvis-modbus-exporter:dlc-main-builder \
 	--build-arg BUILDKIT_INLINE_CACHE=1 .
-docker push jsalverda/jarvis-modbus-exporter:dlc-main-builder
+DOCKER_BUILDKIT=1 docker push jsalverda/jarvis-modbus-exporter:dlc-main-builder
 
-docker build \
+DOCKER_BUILDKIT=1 docker build \
 	--target runtime \
 	--tag jsalverda/jarvis-modbus-exporter:dlc-main-runtime \
 	--cache-from jsalverda/jarvis-modbus-exporter:dlc-main-planner \
@@ -33,4 +33,4 @@ docker build \
 	--cache-from jsalverda/jarvis-modbus-exporter:dlc-main-builder \
 	--cache-from jsalverda/jarvis-modbus-exporter:dlc-main-runtime \
 	--build-arg BUILDKIT_INLINE_CACHE=1 .
-docker push jsalverda/jarvis-modbus-exporter:dlc-main-runtime
+DOCKER_BUILDKIT=1 docker push jsalverda/jarvis-modbus-exporter:dlc-main-runtime
