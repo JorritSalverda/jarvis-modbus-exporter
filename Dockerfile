99c54ebbc096a240cd71dc69a78d2a9ff07a3d3c
jorrit.salverda@gmail.com <jsalverda@travix.com>
2021-05-22 08:18:10 +0200

    copy previously built cargos from cached layer

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,11 @@
-# from https://www.lpalmieri.com/posts/fast-rust-docker-builds/
-FROM rust:1.52 as planner
-WORKDIR /app
-RUN apt-get update && apt-get install -y --no-install-recommends musl-tools
-RUN rustup target add x86_64-unknown-linux-musl
-RUN cargo install cargo-chef --version 0.1.20
-COPY . .
-RUN cargo chef prepare  --recipe-path recipe.json
-
-FROM rust:1.52 as cacher
-WORKDIR /app
-RUN apt-get update && apt-get install -y --no-install-recommends musl-tools
-RUN rustup target add x86_64-unknown-linux-musl
-RUN cargo install cargo-chef --version 0.1.20
-COPY --from=planner /app/recipe.json recipe.json
-COPY --from=jsalverda/jarvis-lib-builder:0.1.16 /app/target target
-COPY --from=jsalverda/jarvis-lib-builder:0.1.16 /usr/local/cargo /usr/local/cargo
-RUN cargo chef cook --release --target x86_64-unknown-linux-musl --recipe-path recipe.json
-
 FROM rust:1.52 as builder
+WORKDIR /app
 RUN apt-get update && apt-get install -y --no-install-recommends musl-tools
 RUN rustup target add x86_64-unknown-linux-musl
-WORKDIR /app
 COPY . .
-COPY --from=cacher /app/target target
-COPY --from=cacher /usr/local/cargo /usr/local/cargo
+# add following 2 lines after initial build to speed up next builds
+COPY --from=jsalverda/jarvis-modbus-exporter:dlc-builder /app/target target
+COPY --from=jsalverda/jarvis-modbus-exporter:dlc-builder /usr/local/cargo /usr/local/cargo
 RUN cargo test --release --target x86_64-unknown-linux-musl
 RUN cargo build --release --target x86_64-unknown-linux-musl
 
--- dlc.sh
+++ dlc.sh
@@ -2,35 +2,15 @@
 set -e
 
 DOCKER_BUILDKIT=1 docker build \
-	--target planner \
-	--tag jsalverda/jarvis-modbus-exporter:dlc-main-planner \
-	--cache-from jsalverda/jarvis-modbus-exporter:dlc-main-planner \
-	--build-arg BUILDKIT_INLINE_CACHE=1 .
-DOCKER_BUILDKIT=1 docker push jsalverda/jarvis-modbus-exporter:dlc-main-planner
-
-DOCKER_BUILDKIT=1 docker build \
-	--target cacher \
-	--tag jsalverda/jarvis-modbus-exporter:dlc-main-cacher \
-	--cache-from jsalverda/jarvis-modbus-exporter:dlc-main-planner \
-	--cache-from jsalverda/jarvis-modbus-exporter:dlc-main-cacher \
-	--build-arg BUILDKIT_INLINE_CACHE=1 .
-DOCKER_BUILDKIT=1 docker push jsalverda/jarvis-modbus-exporter:dlc-main-cacher
-
-DOCKER_BUILDKIT=1 docker build \
 	--target builder \
-	--tag jsalverda/jarvis-modbus-exporter:dlc-main-builder \
-	--cache-from jsalverda/jarvis-modbus-exporter:dlc-main-planner \
-	--cache-from jsalverda/jarvis-modbus-exporter:dlc-main-cacher \
-	--cache-from jsalverda/jarvis-modbus-exporter:dlc-main-builder \
+	--tag jsalverda/jarvis-modbus-exporter:dlc-builder \
+	--cache-from jsalverda/jarvis-modbus-exporter:dlc-builder \
 	--build-arg BUILDKIT_INLINE_CACHE=1 .
-DOCKER_BUILDKIT=1 docker push jsalverda/jarvis-modbus-exporter:dlc-main-builder
+DOCKER_BUILDKIT=1 docker push jsalverda/jarvis-modbus-exporter:dlc-builder
 
 DOCKER_BUILDKIT=1 docker build \
-	--target runtime \
-	--tag jsalverda/jarvis-modbus-exporter:dlc-main-runtime \
-	--cache-from jsalverda/jarvis-modbus-exporter:dlc-main-planner \
-	--cache-from jsalverda/jarvis-modbus-exporter:dlc-main-cacher \
-	--cache-from jsalverda/jarvis-modbus-exporter:dlc-main-builder \
-	--cache-from jsalverda/jarvis-modbus-exporter:dlc-main-runtime \
+	--tag jsalverda/jarvis-modbus-exporter:dlc \
+	--cache-from jsalverda/jarvis-modbus-exporter:dlc-builder \
+	--cache-from jsalverda/jarvis-modbus-exporter:dlc \
 	--build-arg BUILDKIT_INLINE_CACHE=1 .
-DOCKER_BUILDKIT=1 docker push jsalverda/jarvis-modbus-exporter:dlc-main-runtime
+DOCKER_BUILDKIT=1 docker push jsalverda/jarvis-modbus-exporter:dlc
